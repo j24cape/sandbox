2a8a0bf049d352459fb408ea3b5bdc04c05e750b
NISHIZAKI Tadashi <j24cape@gmail.com>
2024-08-09 11:59:36 +0900

    Vonage/PHP:Signature tokenを生成するスクリプトの実装

---
+++ vonage/php/build-signature/build-signature.php
@@ -0,0 +1,21 @@
+<?php
+require_once sprintf('%s/vendor/autoload.php', dirname(__DIR__, 1));
+
+use Vonage\JWT\TokenGenerator;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$application_id = $_ENV['VONAGE_APPLICATION_ID'];
+$application_private_key = $_ENV['VONAGE_APPLICATION_PRIVATE_KEY'];
+
+try {
+    $generator = new TokenGenerator($application_id, $application_private_key);
+    echo $generator->generate(), "\n";
+} catch (Exception $e) {
+    echo sprintf('Exception %s: %s at line %d in %s',
+        get_class($e), $e->getMessage(), $e->getLine(), $e->getFile()), "\n";
+    exit(1);
+}
+
+exit;
